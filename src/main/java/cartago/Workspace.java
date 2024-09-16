5cc53c3da5c4f5784c65307443053830fe1af083
danaivach <danai.vacht@gmail.com>
2024-09-16 13:23:14 +0200

    fix(workspace) ignore handling obs event if artId is null

--- src/main/java/cartago/Workspace.java
+++ src/main/java/cartago/Workspace.java
@@ -1703,7 +1703,9 @@ public class Workspace {
 
 	public void notifyFocusCompleted(ICartagoCallback listener, long actionId, ArtifactId aid, Op op, ArtifactId target, List<ArtifactObsProperty> props) {
 		ActionSucceededEvent ev = eventRegistry.makeFocusActionSucceededEvent(actionId, aid, op, target, props);
-		notifyObsEvent(aid, null, null, props.toArray(new ArtifactObsProperty[0]), null);
+		if (aid != null) {
+			notifyObsEvent(aid, null, null, props.toArray(new ArtifactObsProperty[0]), null);
+		}
 		listener.notifyCartagoEvent(ev);
 	}
 
